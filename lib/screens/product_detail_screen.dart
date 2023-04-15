a12e3ce6232f89dfd04691afa6b1335bfe94ffde
HiBorn4 <hiborn4@gmail.com>
2023-04-16 02:11:46 +0530

    Working on Shopping Cart

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/product_provider.dart';
+import 'package:provider/provider.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  // final String title;
+  // final double price;
+
+  // ProductDetailScreen(this.title, this.price);
+  static const routeName = '/product-detail';
+
+  const ProductDetailScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context).findById(productId);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import '../providers/product_provider.dart';
+import 'product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  const ProductsGrid({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        value: products[i],
+        child: const ProductItem(
+          // products[i].id,
+          // products[i].title,
+          // products[i].imageUrl,
+        ),
+      ),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
